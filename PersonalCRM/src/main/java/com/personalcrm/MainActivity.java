dccfb3df9ea4eb8652d790d14784d170061216a9
benjdu34 <benjamin.grellet@gmail.com>
2014-04-14 18:32:55 +0200

    Benjamin Ajout view Stock 14/04/14

--- PersonalCRM/src/main/java/com/personalcrm/MainActivity.java
+++ PersonalCRM/src/main/java/com/personalcrm/MainActivity.java
@@ -37,5 +37,56 @@ public class MainActivity extends Activity {
     }
 
 
+    public static class StockActivity extends ActionBarActivity {
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.stock_activity);
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.container, new PlaceholderFragment())
+                        .commit();
+            }
+        }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.stock, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            switch (item.getItemId()) {
+                case R.id.action_settings:
+                    return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        /**
+         * A placeholder fragment containing a simple view.
+         */
+        public static class PlaceholderFragment extends Fragment {
+
+            public PlaceholderFragment() {
+            }
+
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                    Bundle savedInstanceState) {
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                return rootView;
+            }
+        }
+
+    }
 }
