2941bb8958060c33b00070ca8a541f57140d4358
Guillaume <guillaume.bistour@gmail.com>
2014-04-15 10:58:28 +0200

    Activity principale pour contact

--- PersonalCRM/src/main/java/com/personalcrm/ContactMainActivity.java
+++ PersonalCRM/src/main/java/com/personalcrm/ContactMainActivity.java
@@ -1,13 +1,9 @@
 package com.personalcrm;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class ContactMainActivity extends Activity{
 
